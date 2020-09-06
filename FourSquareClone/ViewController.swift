7ddd9f0db9366dd8c926a85bdc11e0974cff6c94
Ravisara S. Kariyawasam <ravisara@gmail.com>
2020-09-06 18:12:50 +0100

    Initial Commit

---
+++ FourSquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSquareClone
+//
+//  Created by StarChanger on 06/09/2020.
+//  Copyright © 2020 StarChanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
