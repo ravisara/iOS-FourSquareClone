db8471a495e7c42463aa0c7d86dad2e82472852a
Ravisara S. Kariyawasam <ravisara@gmail.com>
2020-10-24 14:16:57 +0100

    Adds a new View Controller and a MapViewKit

---
+++ FourSquareClone/MapVC.swift
@@ -0,0 +1,30 @@
+//
+//  MapVC.swift
+//  FourSquareClone
+//
+//  Created by StarChanger on 24/10/2020.
+//  Copyright © 2020 StarChanger. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
